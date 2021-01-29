501db8e3786bea0f48c876df501d45b7afc4b6b6
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-01-29 15:04:26 +0300

    use hoc and connect function from redux to change state in reducer and to load elements from db

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,19 +1,44 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import MenuListItem from '../menu-list-item';
+import WithRestoService from '../hoc'
 
 import './menu-list.scss';
 
 class MenuList extends Component {
+    componentDidMount() {
+        const {RestoService, menuLoaded} = this.props;
+        RestoService.getMenuItems()
+            .then(res => menuLoaded(res));
+    }
 
     render() {
+        const {menuItems} = this.props;
 
         return (
             <ul className="menu__list">
-                <MenuListItem/>
+                {
+                    menuItems.map(menuItem => {
+                        return <MenuListItem key={menuItem.id} menuItem={menuItem}/>
+                    })
+                }
             </ul>
         )
     }
 };
 
+const mapStateToProps = (state) => ({menuItems: state.menu})
 
-export default MenuList;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        menuLoaded: (newMenu) => {
+            dispatch({
+                type: 'MENU_LOADED',
+                payload: newMenu
+            })
+        }
+    }
+}
+
+
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
