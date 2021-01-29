bd97a1b64236752c39ca67118a5ecf8761b4ea57
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-01-29 14:36:53 +0300

    add Router

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -6,10 +6,10 @@ import './app-header.scss';
 const AppHeader = ({total}) => {
     return (
         <header className="header">
-            <Link className="header__link" to="#">
+            <Link className="header__link" to={'/'}>
                 Menu
             </Link>
-            <Link className="header__link" to="#">
+            <Link className="header__link" to="/cart">
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
                 Total: {total} $
             </Link>
