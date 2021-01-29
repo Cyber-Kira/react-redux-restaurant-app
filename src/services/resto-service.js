23a0d178fca3979d7406d86b33d9a06e16e46846
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-01-29 14:10:53 +0300

    add fetch to json-server db

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,13 @@
 export default class RestoService {
-    getMenuItems() {
-        return [];
+    async getMenuItems() {
+        const url = 'http://localhost:3000/menu'
+        
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
     }
 }
