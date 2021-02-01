7f4032d9fa31d36b5fce6107fa55d1371bc5958f
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-01 21:45:35 +0300

    add Link component, remove onClick function

--- src/components/menu-list-item/menu-list-item.js
+++ src/components/menu-list-item/menu-list-item.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './menu-list-item.scss';
 
-const MenuListItem = ({menuItem, onItemSelected}) => {
+const MenuListItem = ({menuItem}) => {
     const {title, price, url, category, icon, id} = menuItem;
     return (
-        <li
-        className="menu__item"
-        onClick={() => onItemSelected(id, title)}>
-            <div className="menu__title">{title}</div>
-            <img className="menu__img" src={url} alt={title}></img>
-            <div className="menu__category">Category: <span>{category}</span>
-                <div className="menu__icon"><img src={icon} alt={category}></img></div>
-            </div>
-            <div className="menu__price">Price: <span>{price}$</span></div>
-            <button className="menu__btn">Add to cart</button>
-        </li>
+            <>
+                <li className="menu__item">
+                    <Link to = {`/${id}`}>
+                        <div className="menu__title">{title}</div>
+                        <img className="menu__img" src={url} alt={title}></img>
+                        <div className="menu__category">Category: <span>{category}</span>
+                            <div className="menu__icon"><img src={icon} alt={category}></img></div>
+                        </div>
+                        <div className="menu__price">Price: <span>{price}$</span></div>
+                        <button className="menu__btn">Add to cart</button>
+                    </Link>
+                </li>
+            </>
     )
 }
 
