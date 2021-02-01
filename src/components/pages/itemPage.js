c912e753382da7bbb9fdbe264dcdbd1f5a8ce334
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-01 21:04:11 +0300

    create page for every menu item

---
+++ src/components/pages/itemPage.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import WithRestoService from '../hoc'
+import {menuLoaded, menuRequested, menuError} from '../../actions';
+import Spinner from '../spinner';
+import Error from '../error';
+
+class ItemPage extends Component {
+    componentDidMount() {
+        this.props.menuRequested();
+
+        const {RestoService, menuLoaded, menuError} = this.props;
+        RestoService.getMenuItems()
+            .then(res => menuLoaded(res))
+            .catch(menuError());
+    }
+
+    render() {
+        const {menuItems, loading, error, ItemId} = this.props;
+
+        if (error) {
+            return <Error />
+        }
+
+        if (loading) {
+            return <Spinner />
+        }
+
+        const item = menuItems[ItemId - 1];
+
+        return (
+            View(item)
+        )
+    }
+};
+
+const mapStateToProps = (state) => {
+    return {
+        menuItems: state.menu,
+        loading: state.loading,
+        error: state.error
+    }
+}
+
+const mapDispatchToProps = {
+    menuLoaded,
+    menuRequested,
+    menuError
+};
+
+const View = (item) => {
+    const {title, price, url, category, icon} = item;
+
+    return <li className="menu__item">
+                <div className="menu__title">{title}</div>
+                <img className="menu__img" src={url} alt={title}></img>
+                <div className="menu__category">Category: <span>{category}</span>
+                    <div className="menu__icon"><img src={icon} alt={category}></img></div>
+                </div>
+                <div className="menu__price">Price: <span>{price}$</span></div>
+                <button className="menu__btn">Add to cart</button>
+            </li>
+}
+
+
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(ItemPage));
