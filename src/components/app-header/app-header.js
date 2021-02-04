0dbb3922a142018790d752b49fe79832fd9c0807
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-04 22:14:18 +0300

    connect component with state, using price from state

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import cartIcon from './shopping-cart-solid.svg';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './app-header.scss';
 
-const AppHeader = ({total}) => {
+const AppHeader = ({totalPrice}) => {
     return (
         <header className="header">
             <Link className="header__link" to={'/'}>
@@ -11,10 +12,16 @@ const AppHeader = ({total}) => {
             </Link>
             <Link className="header__link" to="/cart">
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
-                Total: {total} $
+                Total: {totalPrice} $
             </Link>
         </header>
     )
 };
 
-export default AppHeader;
+const mapStateToProps = ({totalPrice}) => {
+    return {
+        totalPrice
+    }
+};
+
+export default connect(mapStateToProps)(AppHeader);
